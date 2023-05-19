a1a578e75ad46730c145c943644bab3dc5eb7415
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2023-05-19 13:06:23 +0300

    Fix trpc Error (createProxySSGHelpers deprecated)

--- src/server/helpers/generateSSGHelper.ts
+++ src/server/helpers/generateSSGHelper.ts
@@ -1,11 +1,11 @@
-import { createProxySSGHelpers } from "@trpc/react-query/ssg";
+import { createServerSideHelpers } from "@trpc/react-query/server";
 import superjson from "superjson";
 
 import { appRouter } from "~/server/api/root";
 import { prisma } from "~/server/db";
 
 const generateSSGHelper = () =>
-  createProxySSGHelpers({
+  createServerSideHelpers({
     router: appRouter,
     ctx: { prisma, userId: null },
     transformer: superjson, // optional - adds superjson serialization
