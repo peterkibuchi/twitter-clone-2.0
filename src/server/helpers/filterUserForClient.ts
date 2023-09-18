0de1154a17688324eaa6f521125fb0b7887a84b1
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2023-09-18 05:35:23 +0300

    Update Clerk, Add `.prettierignore`

--- src/server/helpers/filterUserForClient.ts
+++ src/server/helpers/filterUserForClient.ts
@@ -1,9 +1,9 @@
-import type { User } from "@clerk/nextjs/dist/api";
+import { type User } from "@clerk/nextjs/dist/types/server";
 
 const filterUserForClient = (user: User) => ({
   id: user.id,
   username: user.username,
-  profileImageUrl: user.profileImageUrl,
+  profileImageUrl: user.imageUrl,
 });
 
 export default filterUserForClient;
