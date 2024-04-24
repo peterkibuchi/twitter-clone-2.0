c06533fb176df2c99cda2e1c82d0fc82aef2cce7
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2024-04-24 08:03:11 +0300

    Fix: Modernize tsconfig; Set ES modules as default

---
+++ next.config.js
@@ -0,0 +1,21 @@
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
+ * This is especially useful for Docker builds.
+ */
+!process.env.SKIP_ENV_VALIDATION && (await import("./src/env.js"));
+
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+  images: { domains: ["images.clerk.dev"] },
+
+  /** We already do linting and typechecking as separate tasks in CI */
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+};
+export default config;
